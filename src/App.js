7633635cae427939a0d088faa6840cd3b77219fd
akhilshetty97 <47709446+akhilshetty97@users.noreply.github.com>
2020-12-22 14:15:23 +0530

    Spotify Clone

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import Login from "./Login";
+import { getTokenFromUrl } from "./spotify";
+import SpotifyWebApi from "spotify-web-api-js";
+import Player from "./Player";
+import { useDataLayerValue } from "./DataLayer";
+
+const spotify = new SpotifyWebApi();
 
 function App() {
+  //Run code based on given condition
+  const [token, setToken] = useState(null);
+  const [{ user }, dispatch] = useDataLayerValue();
+
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const _token = hash.access_token;
+    if (_token) {
+      setToken(_token);
+
+      spotify.setAccessToken(_token);
+      spotify.getMe().then((user) => {
+        dispatch({
+          type: "SET_USER",
+          user: user,
+        });
+      });
+
+      spotify.getUserPlaylists().then((playlists) => {
+        dispatch({
+          type: "SET_PLAYLISTS",
+          playlists: playlists,
+        });
+      });
+
+      spotify.getPlaylist("37i9dQZEVXcCHpsOBMfAvz").then((response) => {
+        dispatch({
+          type: "SET_DISCOVER_WEEKLY",
+          discover_weekly: response,
+        });
+      });
+    }
+
+    console.log(user);
+    console.log("Token:", token);
+  }, []); //since we have empty array, whenever page is refreshed condition runs
+  //if we had [name] or something in array, everytime name changes the code runs
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {token ? <Player spotify={spotify} /> : <Login />}
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import "./Header.css";
+import SearchIcon from "@material-ui/icons/Search";
+import { Avatar } from "@material-ui/core";
+import { useDataLayerValue } from "./DataLayer";
+
+function Header() {
+  const [{ user }, dispatch] = useDataLayerValue();
+  return (
+    <div className="header">
+      <div className="header__left">
+        <SearchIcon />
+        <input placeholder="Search for Artists,Songs,Playlists" type="text" />
+      </div>
+      <div className="header__right">
+        <Avatar src={user?.images[0]?.url} alt="AS" />
+        <h4>Akhil Shetty</h4>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
